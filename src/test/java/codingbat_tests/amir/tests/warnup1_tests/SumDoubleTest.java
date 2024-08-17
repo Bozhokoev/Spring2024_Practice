0d595633665dff4449bcd2e73be9b37bee933771
Amirbek <amirbek20162016@gmail.com>
2024-08-17 22:45:21 +0600

    Featury/amir/warmup1/SumDouble

--- src/test/java/codingbat_tests/amir/tests/warnup1_tests/SumDoubleTest.java
+++ src/test/java/codingbat_tests/amir/tests/warnup1_tests/SumDoubleTest.java
@@ -1,21 +1,27 @@
 package codingbat_tests.amir.tests.warnup1_tests;
 
+import codingbat.jyldyzbek.warmup1.SumDouble;
+import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals; // Use Assert from TestNG
 
 public class SumDoubleTest {
 
     @DataProvider(name = "SumDoubleData")
     public Object[][] sumDoubleData() {
         return new Object[][]{
-                {1,2},
-                {2,3} ,
-                {2, 2}
+                {1, 3, 4},   // 1 + 3 = 4
+                {2, 3, 5},   // 2 + 3 = 5
+                {5, 5, 20}    // 2 * (2 + 2) = 8
         };
     }
 
     @Test(dataProvider = "SumDoubleData")
-    public void testSumData(int a, int b){
-        System.out.println("a: " + a + "b: " + b);
+    public void testSumDouble(int a, int b, int expected) {
+        SumDouble sumDouble = new SumDouble();
+        int result = sumDouble.sumDouble(a, b);
+        Assert.assertEquals(result, expected, "Ошибка для входных данных: " + a + " и " + b);
     }
 }

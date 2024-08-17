003de9cd042137eb9dc14bc50ff3f850847c3179
Guldana <danaumar05@gmail.com>
2024-08-18 02:35:30 +0600

    Feature/Guldana/warmup1

--- src/test/java/codingbat_tests/guldana_warmup1_tests/SumDoudleTest.java
+++ src/test/java/codingbat_tests/guldana_warmup1_tests/SumDoudleTest.java
@@ -1,4 +1,4 @@
-package codingbat_tests.tatev.warmup1_rests;
+package codingbat_tests.guldana_warmup1_tests;
 
 import codingbat.tatev.warmup1.SumDouble;
 import org.testng.annotations.DataProvider;
@@ -6,16 +6,15 @@ import org.testng.annotations.Test;
 
 import static org.testng.Assert.assertEquals;
 
-public class SumDoubleTest {
+public class SumDoudleTest {
     @DataProvider(name = "SumDoubleData")
-public Object[][] sumDoubleData() {
-    return new Object[][]{
-            {1, 3, 4},   // 1 + 3 = 4
-            {2, 3, 5},   // 2 + 3 = 5
-            {2, 2, 8}    // 2 * (2 + 2) = 8
-    };
-}
-
+    public Object[][] sumDoubleData() {
+        return new Object[][]{
+                {1, 3, 4},   // 1 + 3 = 4
+                {2, 3, 5},   // 2 + 3 = 5
+                {2, 2, 8}    // 2 * (2 + 2) = 8
+        };
+    }
     @Test(dataProvider = "SumDoubleData")
     public void testSumDouble(int a, int b, int expected) {
         SumDouble sumDouble = new SumDouble();
@@ -23,4 +22,3 @@ public Object[][] sumDoubleData() {
         assertEquals(result, expected, "SumDouble method failed for inputs: " + a + " and " + b);
     }
 }
-

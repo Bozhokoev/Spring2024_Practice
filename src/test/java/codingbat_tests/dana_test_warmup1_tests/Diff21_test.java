10d49e05fff42646ef912837f6134ff4c06e9d1e
Dana <danasdd11@gmail.com>
2024-08-18 14:21:19 +0600

    Feature/Dana/Diff21

---
+++ src/test/java/codingbat_tests/dana_test_warmup1_tests/Diff21_test.java
@@ -0,0 +1,24 @@
+package codingbat_tests.dana_test_warmup1_tests;
+
+import codingbat.dana.warmup1.Diff21;
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class Diff21_test {
+
+    Diff21 diff21 = new Diff21();
+
+    @DataProvider(name = "diff21")
+    public Object [][] dpDiff21(){
+        return new Object[][]{
+                {19,2}, {10,11}, {21,0}, {22,2}, {25,8}, {30,18},
+                {0,21}, {1,20}, {2,19}, {-1,22},
+        };
+    }
+
+    @Test(description =  "Verify Diff21 test", dataProvider = "diff21")
+    public void diff21_test(int a, int res){
+        Assert.assertEquals(diff21.diff21(a), res);
+    }
+}

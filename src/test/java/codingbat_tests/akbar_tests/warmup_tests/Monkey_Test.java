56c855fcf5cd78afa547a20a79d457d3da403eab
Akbarski <akbarqatest@gmail.com>
2024-08-18 12:01:21 +0600

    Monkey_Test

---
+++ src/test/java/codingbat_tests/akbar_tests/warmup_tests/Monkey_Test.java
@@ -0,0 +1,25 @@
+package codingbat_tests.akbar_tests.warmup_tests;
+
+import codingbat.akbar.warmup1.MonkeyTrouble;
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class Monkey_Test {
+    MonkeyTrouble monkeyTest = new MonkeyTrouble();
+
+    @DataProvider(name = "Monkey smile")
+    public Object[][] IsMonkeySmile() {
+        return new Object[][]{
+                {true, true, true},
+                {false, false, true},
+                {true, false, false},
+                {false, true, false}
+        };
+    }
+
+    @Test(description = "Verify is Smile", dataProvider = "Monkey smile")
+    public void SmileMonkey(boolean a, boolean b, boolean exp) {
+        Assert.assertEquals(monkeyTest.monkeyTrouble(a, b), exp);
+    }
+}

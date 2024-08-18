0b33e5e986393bcfa22e85058563dc6335877200
Amirbek <amirbek20162016@gmail.com>
2024-08-18 14:52:17 +0600

    Featury/amir/warmup1/missingChar

---
+++ src/main/java/codingbat/amir/warmup1/MissingChar.java
@@ -0,0 +1,10 @@
+package codingbat.amir.warmup1;
+
+public class MissingChar {
+    public String missingChar(String str, int n) {
+
+        String front = str.substring(0, n);
+        String back = str.substring(n + 1);
+        return front + back;
+    }
+}

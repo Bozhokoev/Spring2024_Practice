2873707f6b45cbf5780e7584fc8482953fd1d5a4
Dana <danasdd11@gmail.com>
2024-08-18 18:00:00 +0600

    Feature/Dana/Warmup1/HasTeen

---
+++ src/main/java/codingbat/dana/warmup1/HasTeen.java
@@ -0,0 +1,9 @@
+package codingbat.dana.warmup1;
+
+public class HasTeen {
+    public boolean hasTeen(int a, int b, int c) {
+        if (a > 12 && a < 20 || b > 12 && b < 20 || c > 12 && c < 20){
+            return true;
+        } return false;
+    }
+}
---
+++ src/test/java/codingbat_tests/dana_test_warmup1_tests/HasTeen_test.java
@@ -0,0 +1,26 @@
+package codingbat_tests.dana_test_warmup1_tests;
+
+import codingbat.dana.warmup1.HasTeen;
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class HasTeen_test {
+
+    HasTeen hasTeen = new HasTeen();
+
+    @DataProvider(name = "hasTeen")
+    public Object[][] dpHasTeen(){
+        return new Object[][]{
+                {13, 20, 10, true},
+                {20, 19, 10, true},
+                {20, 10, 13, true},
+                {1, 20, 12, false},
+                {19, 20, 12, true}
+        };
+    }
+    @Test(description = "Verify Has Teen Functional", dataProvider = "hasTeen")
+    public void hasTeen_test(int a, int b, int c, boolean res){
+        Assert.assertEquals(hasTeen.hasTeen(a, b, c), res);
+    }
+}

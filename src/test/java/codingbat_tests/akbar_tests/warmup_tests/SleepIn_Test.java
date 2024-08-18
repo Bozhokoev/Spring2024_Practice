8406236e0763a073c4c85f857b5a271075a3cbc0
Akbarski <akbarqatest@gmail.com>
2024-08-18 11:57:25 +0600

    SleepIn

---
+++ src/test/java/codingbat_tests/akbar_tests/warmup_tests/SleepIn_Test.java
@@ -0,0 +1,28 @@
+package codingbat_tests.akbar_tests.warmup_tests;
+
+import codingbat.akbar.warmup1.SleepIn;
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+
+public class SleepIn_Test {
+    SleepIn sleepIn = new SleepIn();
+
+    @DataProvider(name = "SleepIn True")
+    public Object[][] dpsleepInTrue() {
+        return new Object[][]{
+                {false, false, true},
+                {true, false, false},
+                {false, true, true},
+                {true, true, true}
+        };
+    }
+
+    @Test(description = "Verify SleepInTest", dataProvider = "SleepIn True")
+    public void testSleepIn(boolean a, boolean b, boolean exp) {
+        Assert.assertEquals(sleepIn.sleepIn(a, b), exp);
+    }
+
+}
+

9cb0f7caa374813ee9f02294f429cf93d1fed311
akim <bozhokoev@gmail.com>
2024-08-18 16:15:42 +0600

    Feature/akim/warmup1/MissingChar

---
+++ src/test/java/codingbat_tests/akim_tests/warmup1_tests/MissingChar_Test.java
@@ -0,0 +1,27 @@
+package codingbat_tests.akim_tests.warmup1_tests;
+
+import codingbat.akim.warmup1.MissingChar;
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class MissingChar_Test {
+    MissingChar missingChar = new MissingChar();
+
+    @DataProvider(name = "missing_char")
+    public Object[][] dpMissingChar(){
+        return new Object[][]{
+                {"kitten", 1, "ktten"},
+                {"kitten", 0, "itten"},
+                {"kitten", 4, "kittn"},
+                {"Hi", 0, "i"},
+                {"code", 3, "cod"},
+        };
+    }
+
+    @Test(description = "Verify missing char function", dataProvider = "missing_char")
+    public void missingChar(String str, int index, String expected){
+        Assert.assertEquals(missingChar.missingChar(str, index), expected);
+    }
+
+}

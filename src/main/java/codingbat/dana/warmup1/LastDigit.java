2a72cb2ea7766968b333cc56b86229333a00338c
Dana <danasdd11@gmail.com>
2024-08-18 19:09:42 +0600

    Feature/Dana/Warmup1/LastDigit

---
+++ src/main/java/codingbat/dana/warmup1/LastDigit.java
@@ -0,0 +1,9 @@
+package codingbat.dana.warmup1;
+
+public class LastDigit {
+    public boolean lastDigit(int a, int b) {
+        if(a % 10 == b %10){
+            return true;
+        } return false;
+    }
+}

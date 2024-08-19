b271031cc1967e2787dc1def0dec7fdb33a4f8b7
Victoria <rvictoriakg@gmail.com>
2024-08-19 14:46:57 +0600

    Feature/vika/warmup2/Has271

---
+++ src/test/java/codingbat_tests/vika_tests/warmup2/Has271_Test.java
@@ -0,0 +1,27 @@
+package codingbat_tests.vika_tests.warmup2;
+
+import codingbat.vika.warmup2.Has271;
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class Has271_Test {
+    Has271 has271 = new Has271();
+
+    @DataProvider(name = "has_271")
+    public Object[][] dpHas271() {
+        return new Object[][] {
+                {new int[] {1, 2, 7, 1}, true},
+                {new int[] {1, 2, 8, 1}, false},
+                {new int[] {3, 8, 2}, true},
+                {new int[] {2, 7, 4}, false},
+                {new int[] {2, 7, 5, 10, 4}, true}
+        };
+    }
+
+    @Test(description = "Verify Has 271 Function", dataProvider = "has_271")
+    public void noHas271Test (int nums[], boolean exp) {
+        Assert.assertEquals(has271.has271(nums), exp);
+    }
+}
+

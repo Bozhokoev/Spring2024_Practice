fc825c99aea1bf7783c521ac78f45479cbe23cd8
Zarina <andarbekovazarina@gmail.com>
2024-08-19 12:45:20 +0600

    Feature/zarina/warmup2/StringBits

---
+++ src/test/java/codingbat_tests/zarina2_tests/warmup2_tests/StringBits_Test.java
@@ -0,0 +1,25 @@
+package codingbat_tests.zarina2_tests.warmup2_tests;
+
+import codingbat.zarina2_warmup2.StringBits;
+import com.beust.ah.A;
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class StringBits_Test {
+    StringBits stringBits = new StringBits();
+
+    @DataProvider (name = "String Bits")
+public Object [][] dpStringBits(){
+        return new Object[][]{
+                {"Hello", "Hlo"},
+                {"", ""},
+                {"pi", "p"},
+        };
+    }
+
+    @Test(description = "Verify String Bts function",dataProvider = "String Bits")
+    public void stringBitsTest(String str, String res){
+        Assert.assertEquals(stringBits.stringBits(str),res);
+    }
+}

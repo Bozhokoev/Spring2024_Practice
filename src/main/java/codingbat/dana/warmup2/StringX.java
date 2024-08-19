c6a3beca7981f17dd934187e4c788252ae4cdeda
Dana <danasdd11@gmail.com>
2024-08-19 13:48:22 +0600

    Feature/Dana/Warmup2/StringX

---
+++ src/main/java/codingbat/dana/warmup2/StringX.java
@@ -0,0 +1,13 @@
+package codingbat.dana.warmup2;
+
+public class StringX {
+    public String stringX(String str) {
+        String result = "";
+        for (int i=0; i<str.length(); i++) {
+            if (!(i > 0 && i < (str.length()-1) && str.substring(i, i+1).equals("x"))) {
+                result = result + str.substring(i, i+1); // Could use str.charAt(i) here
+            }
+        }
+        return result;
+    }
+}

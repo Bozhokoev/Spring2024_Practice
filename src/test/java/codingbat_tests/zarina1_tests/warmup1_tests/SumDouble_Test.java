e50c91358cd8d128c2571e46c98d664c6ac931b8
Zarina <andarbekovazarina@gmail.com>
2024-08-19 15:52:30 +0600

    Feature/zarina/warmup1/SumDouble

---
+++ src/test/java/codingbat_tests/zarina1_tests/warmup1_tests/SumDouble_Test.java
@@ -0,0 +1,25 @@
+package codingbat_tests.zarina1_tests.warmup1_tests;
+
+import codingbat.zarina1.warmup1.SumDouble;
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class SumDouble_Test {
+
+    SumDouble sumDouble = new SumDouble();
+
+    @DataProvider(name = "SumDoubleData")
+    public Object[][] dpSumDouble() {
+        return new Object[][]{
+                {1, 2, 3},
+                {3, 2, 5},
+                {2, 2, 8},
+        };
+    }
+
+    @Test(description = "SumDoubleData", dataProvider = "SumDoubleData")
+    public void setSumDoubleTest(int a, int b, int expected) {
+        Assert.assertEquals(sumDouble.sumDouble(a, b), expected);
+    }
+}

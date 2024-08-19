24ff7cdfac5fcc5f5b0a2177bdf2a4c62b47a66c
Aika <jakudry04@gmail.com>
2024-08-19 14:03:56 +0600

    Feature/janat/warmup2/ArrayFront9

---
+++ src/test/java/codingbat_tests/janat_test_warmup2_tests/ArrayFront9_Test.java
@@ -0,0 +1,28 @@
+package codingbat_tests.janat_test_warmup2_tests;
+
+import codingbat.janat.warmup2.ArrayCount9;
+import codingbat.janat.warmup2.ArrayFront9;
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class ArrayFront9_Test {
+
+    ArrayFront9 arrayFront9 = new ArrayFront9();
+
+    @DataProvider(name = "array_front_9")
+    public Object[][] dpArrayFront9(){
+        return new Object[][]{
+                {new int[] {1, 2, 9, 3, 4}, true},
+                {new int[] {1, 2, 3, 4, 9}, false},
+                {new int[] {1, 2, 3, 4, 5}, false},
+                {new int[] {9, 2, 3}, true},
+                {new int[] {1, 9, 9}, true},
+        };
+    }
+
+    @Test(description = "Verify Array_Front_9",dataProvider = "array_front_9")
+    public void arrayFront9Test(int[] a, boolean exp){
+        Assert.assertEquals(arrayFront9.arrayFront9(a), exp);
+    }
+}

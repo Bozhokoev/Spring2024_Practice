ddd4212c1cc08f46a65e8133468af7cebac1fb03
Dana <danasdd11@gmail.com>
2024-08-20 12:33:25 +0600

    Feature/Dana/String1/Left2

---
+++ src/main/java/codingbat/dana/string1/Left2.java
@@ -0,0 +1,10 @@
+package codingbat.dana.string1;
+
+public class Left2 {
+
+    public String left2(String str) {
+        String left = str.substring(0,2);
+        String left2 = str.substring(2);
+        return left2 + left;
+    }
+}

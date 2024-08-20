fb00ba7820a58c791b8ce5ed4bd326a4538df681
Zarina <andarbekovazarina@gmail.com>
2024-08-20 10:31:42 +0600

    Feature/zarina/warmup1/NotString

---
+++ src/main/java/codingbat/zarina1/warmup1/NotString.java
@@ -0,0 +1,12 @@
+package codingbat.zarina1.warmup1;
+
+public class NotString {
+    public String notString(String str) {
+        if (str.startsWith("not")) {
+            return str;
+        } else {
+            return "not " + str;
+        }
+    }
+
+}
---
+++ src/test/java/codingbat_tests/zarina1_tests/warmup1_tests/NotString_Test.java
@@ -0,0 +1,31 @@
+package codingbat_tests.zarina1_tests.warmup1_tests;
+
+import codingbat.zarina1.warmup1.NotString;
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class NotString_Test {
+
+    NotString notString = new NotString();
+
+    @DataProvider(name = "notString")
+    public Object [][] dpNotString(){
+        return new Object[][]{
+                {"candy", "not candy"},
+                {"x", "not x"},
+                {"not bad", "not bad"},
+                {"bad", "not bad"},
+                {"not", "not"},
+                {"is not", "not is not"},
+                {"no", "not no"}
+        };
+    }
+
+    @Test(description = "Verify Not String", dataProvider = "notString")
+    public void notString_test(String str, String res){
+        Assert.assertEquals(notString.notString(str), res);
+    }
+}
+
+

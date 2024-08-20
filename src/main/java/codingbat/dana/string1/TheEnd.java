50df7dc9238d1e4389df46180c2c7729ca3863df
Dana <danasdd11@gmail.com>
2024-08-20 12:46:33 +0600

    Feature/Dana/String1/TheEnd

---
+++ src/main/java/codingbat/dana/string1/TheEnd.java
@@ -0,0 +1,10 @@
+package codingbat.dana.string1;
+
+public class TheEnd {
+
+    public String theEnd(String str, boolean front) {
+        String st = str.substring(0,1);
+        String st2 = str.substring(str.length()-1);
+        return front ? st : st2;
+    }
+}

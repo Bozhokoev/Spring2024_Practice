a72fd1878b459b4d08c0df415eaeb5fcfbbace6a
Zarina <andarbekovazarina@gmail.com>
2024-08-22 00:48:49 +0600

    Feature/zarina/warmup1/Front22

---
+++ src/test/java/codingbat_tests/zarina1_tests/warmup1_tests/Front22_Test.java
@@ -0,0 +1,29 @@
+package codingbat_tests.zarina1_tests.warmup1_tests;
+
+import codingbat.zarina1.warmup1.Front22;
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class Front22_Test {
+
+    Front22 front22 = new Front22();
+
+    @DataProvider(name = "front22")
+    public Object[][] dpFront22() {
+        return new Object[][]{
+                {"kitten", "kikittenki"},
+                {"Ha", "HaHaHa"},
+                {"abc", "ababcab"},
+                {"ab", "ababab"},
+                {"a", "aaa"},
+                {"", ""},
+                {"Logic", "LoLogicLo"},
+        };
+    }
+
+    @Test(description = "Verify front22 Functional", dataProvider = "front22")
+    public void front22_test(String str, String res) {
+        Assert.assertEquals(front22.front22(str), res);
+    }
+}

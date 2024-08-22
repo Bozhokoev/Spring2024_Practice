fbb810d74eaa09cd99d28944aab9fe0825bcb37e
Syrgak <syrgakkenzhebaev@gmail.com>
2024-08-22 07:05:52 +0600

    Feature/Syrgak/Warmup/Close10

---
+++ src/main/java/codingbat/syrgak1/warmup1/Close10.java
@@ -0,0 +1,15 @@
+package codingbat.syrgak1.warmup1;
+
+public class Close10 {
+    public int close10(int a, int b) {
+        int aDiff = Math.abs(a-10);
+        int bDiff = Math.abs(b-10);
+        if(aDiff < bDiff){
+            return a;
+        }
+        if(aDiff > bDiff){
+            return b;
+        }
+        return 0;
+    }
+}

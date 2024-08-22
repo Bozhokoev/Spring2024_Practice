71d21c9ddc1e83086bc07586effb7a5e98030fc8
Syrgak <syrgakkenzhebaev@gmail.com>
2024-08-22 07:27:53 +0600

    Feature/Syrgak/Warmup/StringE

---
+++ src/main/java/codingbat/syrgak1/warmup1/StringE.java
@@ -0,0 +1,12 @@
+package codingbat.syrgak1.warmup1;
+
+public class StringE {
+    public boolean stringE(String str) {
+        int num = 0;
+        for(int i = 0; i < str.length(); i++) {
+            if(str.charAt(i) == 'e')
+                num++;
+        }
+        return 1 <= num && num <= 3;
+    }
+}

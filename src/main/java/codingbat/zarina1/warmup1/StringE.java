4757349e6a9ce140002edd150cde57fe337a97cd
Zarina <andarbekovazarina@gmail.com>
2024-08-22 20:30:47 +0600

    Feature/zarina/warmup1/StringE

---
+++ src/main/java/codingbat/zarina1/warmup1/StringE.java
@@ -0,0 +1,17 @@
+package codingbat.zarina1.warmup1;
+
+public class StringE {
+
+    public boolean stringE(String str) {
+        int count = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == 'e') count++;
+        }
+        return (count >= 1 && count <= 3);
+    }
+
+
+}
+
+
